80c229474120f68475fc6eeeb4f60deac76e130f
Dusan Krtinic <dule147@gmail.com>
2024-11-19 11:45:25 +0100

    Finish_serri

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
-$conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+// $conn = mysqli_connect('localhost','root','','shop_db') or die('connection failed');
+$conn = mysqli_connect('173.212.235.205','dusancodefactory_root','ik;-}DVM^^v+','dusancodefactory_winterworld') or die('connection failed');
 
 ?>
